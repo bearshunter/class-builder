e31c6b0a35a57f707cc3c08290119b281c218447
Dmytro Prokopenko <dpo@encodify.com>
2025-06-23 23:10:44 +0300

    add files

---
+++ src/main/java/my/prokopenkodi/classbuilder/ClassBuilderApplication.java
@@ -0,0 +1,13 @@
+package my.prokopenkodi.classbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassBuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/prokopenkodi/classbuilder/ClassBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package my.prokopenkodi.classbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassBuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
